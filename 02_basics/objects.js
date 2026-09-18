8cd8f5f0bb372023bc7f8439f9bab7726ce05ff6
HamzaTariq2 <hamzatariqprofessional@gmail.com>
2026-09-18 11:10:54 +0000

    Objects Part 1

---
+++ 02_basics/objects.js
@@ -0,0 +1,21 @@
+//When we declare an object through literals, its a singleton. From a constructor, it is multiple instances of an object.
+//Object.create(making multiple instances of and object through a constructor)
+
+//object literals (the usual way):
+const mySymbol=Symbol() //to make symbol a part of an object, we must decalre it outside an object first
+const person={
+    name:"Hamza",
+    email:"hamzatariqprofessional@gmail.com",
+    isLOggedIn:false,
+    [mySymbol]:"Key1" //This is a symbol
+}
+console.log("***************Correct way to access an object's values**************************")
+console.log("My email from the object: ",person["email"])
+console.log("My symbol",person[mySymbol]) //key will not be a string
+console.log("Type of mySymbol", typeof person[mySymbol])
+//Object.freeze(person) //Any key/value in the object cannot be changed
+
+//Introducing a new func in an object and using 'this' command
+person.greeting=function(){
+    console.log(`Hello ${this.name}`)} //arrow functions do not have their own 'this' keyword, so we cannot use them here
+console.log(person.greeting()) //Hello Hamza
